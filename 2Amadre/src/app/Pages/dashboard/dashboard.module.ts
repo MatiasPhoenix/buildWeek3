8096286f9133f3ea8da4448234d3f163b783dbd1
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-12-11 17:07:29 +0100

    Rotta Pagina Preferiti

--- 2Amadre/src/app/Pages/dashboard/dashboard.module.ts
+++ 2Amadre/src/app/Pages/dashboard/dashboard.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { PreferitiComponent } from './preferiti/preferiti.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PreferitiComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
   ]
 })
 export class DashboardModule { }
