7cff8dc3c2276721d847f16a9ac14b27d0cc569d
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-12-15 12:09:53 +0100

    completamento modifica user-page

--- 2Amadre/src/app/Pages/user-page/user-page.component.ts
+++ 2Amadre/src/app/Pages/user-page/user-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { iUser } from '../auth/Models/i-user';
 import { AuthService } from '../auth/auth.service';
 import { Imovie } from '../dashboard/models/imovie';
+import { Observable } from 'rxjs';
+import { iAccessData } from '../auth/Models/i-access-data';
 
 @Component({
   selector: 'app-user-page',
@@ -15,6 +17,16 @@ export class UserPageComponent {
   username!     : string
   allFavorites  : Imovie[] = []
   allUsernames  : string[] = ['test1', 'prova2', 'tuofiglio', 'testicolo2', 'bigtest5'];
+  movieWatch   : number = this.getRandomWatchCount();
+  serieWatch   : number = this.getRandomWatchCount();
+  form :boolean = false;
+  nome!:string
+  email!:string
+
+  // currentUser: iUser={
+  //   nome
+  //   email,
+  // }
 
   constructor(private authSvc : AuthService) {}
 
@@ -25,7 +37,7 @@ export class UserPageComponent {
       this.test()
     })
     this.username = this.getRandomUsername();
-    this.profilePic = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
+    this.profilePic = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp';
   }
 
   test() {
@@ -38,11 +50,32 @@ export class UserPageComponent {
     return this.allUsernames[randomIndex];
   }
 
+  getRandomWatchCount(){
+    const randomIndex = Math.floor(Math.random() * 100);
+    return randomIndex
+  }
+
   loadFavorites() {
     this.allFavorites = [];
     if (this.currentUser && this.currentUser.favorites) {
       this.allFavorites = [...this.currentUser.favorites];
       this.allFavorites.splice(3);
     }
+  }
+
+  modificaProfilo(){
+    this.form = !this.form
+  }
+  updateData(){
+    if(this.nome){
+      this.currentUser.nome = this.nome
+    }
+    if(this.email){
+      this.currentUser.email = this.email
+    }
+      this.authSvc.updateData(this.currentUser).subscribe()
+      this.modificaProfilo()
+      this.nome = ''
+      this.email = ''
   }
 }
