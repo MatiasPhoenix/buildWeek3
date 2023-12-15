442d3362d5e1017200815c30b44d27fefeda4d35
MatiasPhoenix <matias.la19@gmail.com>
2023-12-15 16:06:33 +0100

    2Amadre-e'-leggenda

--- 2Amadre/src/app/Pages/auth/login/login.component.ts
+++ 2Amadre/src/app/Pages/auth/login/login.component.ts
@@ -17,8 +17,8 @@ export class LoginComponent {
     ){}
 
   loginData:iLogin = {
-    email    : 'mario@rossi.it',
-    password : 'password'
+    email    : '',
+    password : ''
   }
 
   saveLogin(){
