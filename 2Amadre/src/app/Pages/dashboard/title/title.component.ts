86226525129d26792b184a58c6f50a5128f1cec3
MatiasPhoenix <matias.la19@gmail.com>
2023-12-15 08:56:34 +0100

    film-page-random-consigliati

--- 2Amadre/src/app/Pages/dashboard/title/title.component.ts
+++ 2Amadre/src/app/Pages/dashboard/title/title.component.ts
@@ -10,8 +10,10 @@ import { Imoviedetails } from '../models/imoviedetails';
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
-  votiArr:[] = [];
-  movies: Imovie[] =[
+
+
+  // film speciali 2Amadre
+  movies  : Imovie[] =[
     {
     Title: "Amor de madre",
     Year: "2013",
@@ -46,6 +48,44 @@ export class TitleComponent {
       imdbID: "tt0778641",
       Type: "movie",
       Poster: "https://m.media-amazon.com/images/M/MV5BMmM3ZjM3YWUtYzllZS00YmY1LWFlZmUtNTQ0MGY2ZmQ2ZGU0XkEyXkFqcGdeQXVyNTk0MzkxNDk@._V1_SX300.jpg"
+    }
+  ]
+  // un po' di fantasy e fantascenza
+  movies1 : Imovie[] =[
+    {
+      Title: "Dungeons & Dragons: Honor Among Thieves",
+      Year: "2023",
+      imdbID: "tt2906216",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BZWM5MTQ3NDMtNGFiMS00Y2E5LWE2ZTUtNzM5MTcyZjM3ODRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+    },
+    {
+      Title: "Dungeons & Dragons",
+      Year: "1983-1985",
+      imdbID: "tt0085011",
+      Type: "series",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOWM0MjNkMjEtY2IyNi00ODdmLTk0ODctNjY3YTQzN2Y3N2ZiXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+      Title: "Interstellar",
+      Year: "2014",
+      imdbID: "tt0816692",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+    },
+    {
+      Title: "2001: A Space Odyssey",
+      Year: "1968",
+      imdbID: "tt0062622",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMmNlYzRiNDctZWNhMi00MzI4LThkZTctMTUzMmZkMmFmNThmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+    },
+    {
+      Title: "Star Wars: Episode IV - A New Hope",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
     }
   ]
 
@@ -87,6 +127,7 @@ export class TitleComponent {
     this.route.params.subscribe((params: any) => {
       this.dSvc.getById(params.id).subscribe(res => {
         this.movie = res;
+
         console.log(this.movie)
 
       })
@@ -94,5 +135,21 @@ export class TitleComponent {
 
   }
 
+  // esperimento funzione random film consigliati
+  combinedArray = [...this.movies, ...this.movies1];
+  shuffledArray = this.shuffleAndLimitArray(this.combinedArray, 5);
 
+  shuffleAndLimitArray(array: any[], limit: number): any[] {
+    const newArray = [...array];
+
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];}
+    return newArray.slice(0, limit);
+  }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+    this.shuffledArray = this.shuffleAndLimitArray(this.combinedArray, 5);
+  }
 }
